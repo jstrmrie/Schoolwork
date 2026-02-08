7553766a5814472d532730d2a72f4fee658b31b2
jstrmrie <jestermartinpoblete021@gmail.com>
2026-02-08 09:13:08 +0800

    updated

--- ComputeAverageApp/ComputeAverageApp/Program.cs
+++ ComputeAverageApp/ComputeAverageApp/Program.cs
@@ -6,21 +6,26 @@ namespace ComputeAverageApp
     {
         static void Main(string[] args)
         {
-            double firstGrade, secondGrade, thirdGrade, fourthGrade, fifthGrade;
+            Console.WriteLine(  "┌─────────────────────────────────────────────────────┐" +
+                              "\n│                 COMPUTE AVERAGE APP                 │" +
+                              "\n└─────────────────────────────────────────────────────┘" +
+                              "\n - Enter 5 grades separated by new line" +
+                              "");
+            double sum = 0;
 
-            Console.WriteLine("Enter 5 grades separated by new line");
-            firstGrade = Convert.ToDouble(Console.ReadLine());
-            secondGrade = Convert.ToDouble(Console.ReadLine());
-            thirdGrade = Convert.ToDouble(Console.ReadLine());
-            fourthGrade = Convert.ToDouble(Console.ReadLine());
-            fifthGrade = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(" -> ");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                sum += grade;
+            }
 
-            double average = (firstGrade + secondGrade + thirdGrade + fourthGrade + fifthGrade) / 5;
-
+            double average = sum / 5;
             double roundAverage = Math.Round(average);
 
-            Console.WriteLine("The average is " + average + " and round off to " + roundAverage);
 
+            Console.WriteLine("The average is " + average + " and round off to " + roundAverage);
+                                
             Console.WriteLine("\npress any key to exit...");
             Console.ReadKey();
 
